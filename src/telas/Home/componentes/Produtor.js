7505933acc8c96a07827e0679ae526be6d9514a2
camila-dv@hotmail.com <Am@r0813>
2023-05-24 21:42:09 -0300

    first commit

---
+++ src/telas/Home/componentes/Produtor.js
@@ -0,0 +1,78 @@
+import React, { useMemo, useReducer, useState } from "react";
+import {Text, View, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import Estrelas from '../../../componentes/estrelas';
+
+const distanciaEmMetros = (distancia) => {
+  return `${distancia}m`;
+};
+export default function Produtor({nome, imagem, distancia, estrelas}) {
+  const [selecionado, inverterSelecionado] = useReducer(
+    (selecionado) => !selecionado,
+    false,
+  );
+
+  const distanciaTexto = useMemo(
+    () => distanciaEmMetros(distancia),
+    [distancia],
+  );
+
+  return (
+    <TouchableOpacity
+      style={estilos.cartao}
+      onPress={() => inverterSelecionado(!selecionado)}>
+      <Image style={estilos.imagen} source={imagem} accessibilityLabel={nome} />
+      <View style={estilos.informacoes}>
+        <View>
+          <Text style={estilos.nome}>{nome}</Text>
+          <Estrelas
+            quantidade={estrelas}
+            editavel={selecionado}
+            grande={selecionado}
+          />
+        </View>
+        <Text style={estilos.distancia}>{distanciaTexto}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const estilos= StyleSheet.create({
+  cartao: {
+    backgroundColor: '#F6F6F6',
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 6,
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+  },
+  imagen: {
+    width: 48,
+    height: 48,
+    borderRadius: 6,
+    marginVertical: 16,
+    marginLeft: 16,
+  },
+  informacoes: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: 8,
+    marginVertical: 16,
+    marginRight: 16,
+  },
+  nome: {
+    fontSize: 14,
+    lineHeight: 22,
+    fontWeight: 'bold',
+  },
+  distancia: {
+    fontSize: 12,
+    lineHeight: 19,
+  },
+});
